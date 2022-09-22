23fc396beef6653e213d0b71cab2dfef96057332
Jackie Zhou <32947652+jackie-z528@users.noreply.github.com>
2022-09-22 13:16:30 -0400

    Update fbConfig.js

--- firebase-react/src/Config/fbConfig.js
+++ firebase-react/src/Config/fbConfig.js
@@ -2,14 +2,15 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCY_4uMA05I1fedQySAsOB6EwOPYUVZrRA",
-    authDomain: "fir-connect-e726b.firebaseapp.com",
-    projectId: "fir-connect-e726b",
-    storageBucket: "fir-connect-e726b.appspot.com",
-    messagingSenderId: "35407786422",
-    appId: "1:35407786422:web:e7af8bfee20691685f9117"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBK6JQFhcCDyweyZWeEN3ZNA74FqjxD4Lo",
+  authDomain: "meeting-app-de39c.firebaseapp.com",
+  projectId: "meeting-app-de39c",
+  storageBucket: "meeting-app-de39c.appspot.com",
+  messagingSenderId: "337515599525",
+  appId: "1:337515599525:web:03592486c9faa035e31f4c",
+  measurementId: "G-SK1VW5LV57"
+};
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.firestore().settings({ timestampsInSnapshots: true });
